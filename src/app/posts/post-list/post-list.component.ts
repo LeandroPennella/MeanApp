7faaa934bccaf3cc554bd88da48bc9078bcd6590
leandropennella <leandropennella@yahoo.com.ar>
2018-09-12 12:44:09 -0300

    2.22 CreatingPosts

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent {
+  posts = []
+  // [
+  //   {title: 'title', content: 'content'},
+  //   {title: 'title', content: 'content'},
+  //   {title: 'title', content: 'content'}
+  // ];
+}
