72cd412655c3f20894b438f0f0d4dd8ca3c48f81
leandropennella <leandropennella@yahoo.com.ar>
2018-09-17 15:55:30 -0300

    conflicto2

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output} from '@angular/core';
+import { Post } from '../Post.model';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -7,10 +7,16 @@ import { Component } from '@angular/core';
 
 })
 export class PostCreateComponent {
-  valorIngresado = '';
-  newPost = 'nada';
+  textoIngresado = '';
+  tituloIngresado = '';
+  @Output() postCreated = new EventEmitter<Post>();
+
   onAddPost(postInput: HTMLTextAreaElement) {
     // console.dir(postInput);
-    this.newPost = this.valorIngresado;
+
+    // this.newPost = this.textoIngresado;
+    const post: Post = {titulo: this.tituloIngresado, texto: this.textoIngresado};
+    this.postCreated.emit(post);
+
   }
 }
