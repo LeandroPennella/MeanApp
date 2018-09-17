c48f8347b63b37da92d3267e5a9e845f094e230a
leandropennella <leandropennella@yahoo.com.ar>
2018-09-17 16:36:06 -0300

    2.24 Forms

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output} from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Post } from '../Post.model';
+
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -11,11 +13,17 @@ export class PostCreateComponent {
   tituloIngresado = '';
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost(postInput: HTMLTextAreaElement) {
+  onAddPost(form: NgForm) {
     // console.dir(postInput);
 
     // this.newPost = this.textoIngresado;
-    const post: Post = {titulo: this.tituloIngresado, texto: this.textoIngresado};
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = {
+      titulo: form.value.tituloIngresado,
+      texto: form.value.textoIngresado
+    };
     this.postCreated.emit(post);
 
   }
