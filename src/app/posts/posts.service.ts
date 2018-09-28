a9790054a12bd1f5bcabd51c0f9046bf86c0c483
leandropennella <leandropennella@yahoo.com.ar>
2018-09-28 11:13:48 -0300

    2.26.c errorfix

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,11 +1,11 @@
 import { Post } from './post.model';
 import { Injectable } from '@angular/core';
-import { Subject} from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
-  private postsUpdated = new Subject<Post[]>()
+  private postsUpdated = new Subject<Post[]>();
 
   getPosts() {
     return [...this.posts]; // genera una copia de posts (sino devolveria la referencia)
