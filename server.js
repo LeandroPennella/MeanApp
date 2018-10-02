87d8897a7c410ca384d41f588c498f5edff71d5f
Leandro Pennella <leandropennella@yahoo.com.ar>
2018-10-02 15:14:25 -0300

    3.33.b Node Backend

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log('sarasa');
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  response.end('responses ended');
+});
+
+server.listen (process.env.PORT || 3000);
+
