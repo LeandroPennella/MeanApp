ae06eab1b74033e59771977419d713a3a92c187c
Leandro Pennella <leandropennella@yahoo.com.ar>
2018-10-03 16:18:26 -0300

    3.36 json posts desde servidor

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require ('express');
 
 const app = express();
 
-app.use((request,response, next) =>   {
-  console.log('log 1');
-  next();
-});
-
-app.use((request,response, next) =>   {
-  response.send('sending from xp');
+app.use('/api/posts', (request,response, next) =>   {
+  const posts = [
+    {
+      id:'asd09a8',
+      title:'titulo 1 ',
+      content:'contenido 1'
+    },
+    {
+      id:'rty09rty',
+      title:'titulo 2',
+      content:'contenido 2'
+    }
+  ];
+  return response.status(200).json({
+    message: 'posteos pasados correctamente',
+    posts: posts
+  })
 });
 
 module.exports = app;
