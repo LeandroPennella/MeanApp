315ee5b58fa861b848e0a2d77d93e0478a8fb7d3
leandropennella <leandropennella@envy>
2019-02-01 18:43:22 -0300

    81(6.11) fetchimagesonfrontend

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require ('express');
 const bodyParser = require('body-parser');
 
@@ -31,6 +32,8 @@ mongoose.connect(
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended: false }));
+
+app.use("/images", express.static(path.join("backend/images")))
 
 app.use((request,response, next) =>  {
   response.setHeader("Access-Control-Allow-Origin", "*");
